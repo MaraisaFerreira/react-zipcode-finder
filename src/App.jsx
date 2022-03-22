a77fb59d989c2a693a3cc7cf4455704f8549136a
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-03-22 12:59:33 -0300

    main template

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import './App.css';
 
+import { FiSearch } from 'react-icons/fi';
+
 function App() {
-	return <div className='App'>App</div>;
+	return (
+		<div className='main-container'>
+			<h1 className='title'>ZipCode Finder</h1>
+
+			<div className='search-area'>
+				<input type='text' placeholder='Enter a ZipCode here...' />
+				<button className='btn-search'>
+					<FiSearch size={25} color='#fff' />
+				</button>
+			</div>
+			<div className='result-area'>resultado</div>
+		</div>
+	);
 }
 
 export default App;
