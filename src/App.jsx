cf1501131859e38a36631c8baa65a70fa6137606
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-03-23 11:48:40 -0300

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css';
 
+import { useState } from 'react';
+
 import AppTitle from './Components/AppTitle';
 import ResultArea from './Components/ResultArea';
 import SearchArea from './Components/SearchArea';
 
 function App() {
+	const [input, setInput] = useState('');
+	const [zipCode, setZipCode] = useState({});
+
+	const handleChange = (value) => {
+		setInput(value);
+	};
+
 	return (
 		<div className='main-container'>
 			<AppTitle />
 
-			<SearchArea />
+			<SearchArea value={input} handleChange={handleChange} />
 
 			<ResultArea />
 		</div>
--- src/Components/SearchArea.jsx
+++ src/Components/SearchArea.jsx
@@ -2,11 +2,33 @@ import './SearchArea.css';
 
 import { ImSearch } from 'react-icons/im';
 
-const SearchArea = () => {
+const SearchArea = ({ value, handleChange }) => {
+	const urlBase = 'http://api.zippopotam.us/';
+
+	const handleClick = async () => {
+		if (!value) {
+			alert('Digite um cep na área de busca...');
+			return;
+		}
+
+		try {
+			fetch(urlBase + `BR/05414-000`)
+				.then((resp) => resp.json())
+				.then((json) => console.log(json));
+		} catch (error) {
+			console.log('Not found');
+		}
+	};
+
 	return (
 		<div className='search-area'>
-			<input type='text' placeholder='Enter a ZipCode here...' />
-			<button className='btn-search'>
+			<input
+				type='text'
+				placeholder='Enter a ZipCode here...'
+				value={value}
+				onChange={(e) => handleChange(e.target.value)}
+			/>
+			<button className='btn-search' onClick={(e) => handleClick()}>
 				<ImSearch size={25} color='#fff' />
 			</button>
 		</div>
